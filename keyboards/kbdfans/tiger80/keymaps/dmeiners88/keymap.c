4ee20c5affc3769820ffde22341c164f87510db4
Daniel Meiners <mail@dmeiners.de>
2022-12-03 20:01:43 +0100

    Add missing return statement

--- keyboards/kbdfans/tiger80/keymaps/dmeiners88/keymap.c
+++ keyboards/kbdfans/tiger80/keymaps/dmeiners88/keymap.c
@@ -63,6 +63,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 } else {
                     unregister_code(KC_ESC);
                 }
+                return false;
             }
         default:
             return true;
