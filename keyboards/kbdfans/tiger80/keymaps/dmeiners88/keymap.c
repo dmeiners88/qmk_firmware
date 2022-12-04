491061207df564eb3212bb38b090eaccb15f54f8
Daniel Meiners <mail@dmeiners.de>
2022-12-04 19:46:56 +0100

    Register KC_F24 always when on layer 1

--- keyboards/kbdfans/tiger80/keymaps/dmeiners88/keymap.c
+++ keyboards/kbdfans/tiger80/keymaps/dmeiners88/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-enum my_keycodes { MAGIC = SAFE_RANGE, VDESK, ESC_BOOT };
+enum my_keycodes { VDESK = SAFE_RANGE, ESC_BOOT };
 
 static uint8_t vdesk_state = 0;
 
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  VDESK,   KC_MPLY, KC_MNXT, KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,          KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_INS,  KC_HOME, KC_PGUP,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,          KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_END,  KC_PGDN,
-        MAGIC  , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,          KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        MO(1)  , KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,          KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,          KC_UP,
         KC_LCTL, KC_LGUI, KC_LALT,                            LT(1, KC_SPC),                                     KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -30,15 +30,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case MAGIC:
-            if (record->event.pressed) {
-                register_code(KC_F24);
-                layer_move(1);
-            } else {
-                unregister_code(KC_F24);
-                layer_move(0);
-            }
-            return false;
         case VDESK:
             if (record->event.pressed) {
                 if (vdesk_state) {
@@ -66,4 +57,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         default:
             return true;
     }
+}
+
+layer_state_t layer_state_set_user(layer_state_t state){
+    switch (get_highest_layer(state)) {
+        case 0:
+            unregister_code(KC_F24);
+            break;
+        case 1:
+            register_code(KC_F24);
+            break;
+        default:
+            break;
+    }
+    return state;
 }
